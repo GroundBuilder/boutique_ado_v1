fdbedbdce40f4dbbe489a75fd2b8f71d2f7b3d07
GroundBuilder <>
2023-05-06 20:34:47 +0000

    Add sorting JS and back to top button.

--- products/views.py
+++ products/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
 
-
 def all_products(request):
-    """ A view to return the index page.  """
+    """ A view to show all products, including sorting and search queries. """
 
     products = Product.objects.all()
     query = None
@@ -22,7 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -37,7 +38,7 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search critera")
+                messages.error(request, "You didn't enter any search critera!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
@@ -49,7 +50,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'current_sorting': current_sorting
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
